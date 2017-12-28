a01e264334fc413f2f8df517a193e35cd9471616
Joel Edwards <joeledwards@gmail.com>
2017-12-28 13:12:56 -0700

    feat: introduced linting using standard

--- bin/etcdig.js
+++ bin/etcdig.js
@@ -1,33 +1,45 @@
 #!/usr/bin/env node
 
 const {find, grep, ls} = require('../lib/commands')
+const etcd = require('../etcd')
 
 let cmdInvoked = false
-const cmdObserver = (action) => {
+const cmdObserver = action => {
   return (...args) => {
     console.log('args:', args)
     cmdInvoked = true
     action(...args)
   }
 }
-const argular = ({def, desc, args = (yargs) => yargs, action}) => {
-  return [def, desc, args, cmdObserver(action)]
+const argular = ({def, desc, args = yargs => yargs, action}) => {
+  const storeInjector = options => {
+    const store = etcd.getStore(options)
+    return action({store, ...options})
+  }
+
+  return [def, desc, args, cmdObserver(storeInjector)]
 }
 
 const yargs = require('yargs')
 
-yargs
-.env('ETCDIG')
-.option('nodes', {
-  configParser: nodes => nodes.split(',').map(s => s.trim()).filter(s => s !== ''),
-  description: 'List of etcd cluster nodes',
-  default: 'localhost:2379'
-})
-.command(...argular(find))
-.command(...argular(grep))
-.command(...argular(ls))
-.argv
+function run () {
+  const argv = yargs
+  .env('ETCDIG')
+  .option('nodes', {
+    configParser: nodes => nodes.split(',').map(s => s.trim()).filter(s => s !== ''),
+    description: 'List of etcd cluster nodes',
+    default: 'localhost:2379'
+  })
+  .command(...argular(find))
+  .command(...argular(grep))
+  .command(...argular(ls))
+  .argv
 
-if (!cmdInvoked)
-  yargs.showHelp()
+  if (!cmdInvoked) {
+    yargs.showHelp()
+  }
 
+  return argv
+}
+
+run()
